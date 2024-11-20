75b49e9c384b1f6801f7c6625fccad084b005b4d
joeyshi07 <joeys19@nycstudents.net>
2024-11-20 13:47:57 -0500

    restart

--- app/main.js
+++ app/main.js
@@ -1,18 +1,11 @@
 import "./style.css";
 
 const nasaAPI = "https://images-api.nasa.gov/search?q=&media_type=image";
-fetch(nasaAPI).then((response) => response.json());
 
 async function fetchData(nasaAPI) {
   const response = await fetch(nasaAPI);
   const data = await response.json();
+  document.getElementById("api-response").textContent = data.content;
   return data;
 }
 fetchData(nasaAPI);
-
-const apiResponseDOM = document.getElementById("api-response");
-const putQuoteINHTML = async () => {
-  const quote = await fetchData(apiEntry);
-  apiResponseDOM.innerHTML = `Quote: ${quote.content}`;
-};
-putQuoteINHTML();
